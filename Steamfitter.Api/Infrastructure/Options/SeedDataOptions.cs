e21a9b95bab173332ddb653eab89c8a584f5bf0d
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2022-03-28 11:32:58 -0400

    fixed error when no seed data is set in appsettings
    - set default empty lists for seed data properties

--- Steamfitter.Api/Infrastructure/Options/SeedDataOptions.cs
+++ Steamfitter.Api/Infrastructure/Options/SeedDataOptions.cs
@@ -8,9 +8,9 @@ namespace Steamfitter.Api.Infrastructure.Options
 {
     public class SeedDataOptions
     {
-        public List<PermissionEntity> Permissions { get; set; }
-        public List<UserEntity> Users { get; set; }
-        public List<UserPermissionEntity> UserPermissions { get; set; }
+        public List<PermissionEntity> Permissions { get; set; } = new List<PermissionEntity>();
+        public List<UserEntity> Users { get; set; } = new List<UserEntity>();
+        public List<UserPermissionEntity> UserPermissions { get; set; } = new List<UserPermissionEntity>();
     }
 }
 
