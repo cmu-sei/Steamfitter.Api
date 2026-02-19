66f2a3b8ec475507df353a0406948b4a60a0aa12
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2026-02-19 09:00:35 -0500

    fix xapiQueuedStatement (#68)

--- Steamfitter.Api.Migrations.PostgreSQL/Migrations/20260219135016_XapiFix.Designer.cs
+++ Steamfitter.Api.Migrations.PostgreSQL/Migrations/20260219135016_XapiFix.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Steamfitter.Api.Data;
@@ -11,9 +12,11 @@ using Steamfitter.Api.Data;
 namespace Steamfitter.Api.Migrations.PostgreSQL.Migrations
 {
     [DbContext(typeof(SteamfitterContext))]
-    partial class SteamfitterContextModelSnapshot : ModelSnapshot
+    [Migration("20260219135016_XapiFix")]
+    partial class XapiFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
