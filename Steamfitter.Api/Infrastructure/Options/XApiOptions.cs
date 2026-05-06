8ca22ee71f87fa1739f5b9f1343e76dc3ff035d4
Adam Welle <arwelle@cert.org>
2026-05-06 19:00:24 +0000

    Add Enabled flag to XApiOptions to explicitly disable xAPI tracking

--- Steamfitter.Api/Infrastructure/Options/XApiOptions.cs
+++ Steamfitter.Api/Infrastructure/Options/XApiOptions.cs
@@ -8,6 +8,7 @@ namespace Steamfitter.Api.Infrastructure.Options
 {
     public class XApiOptions
     {
+        public bool Enabled { get; set; }
         public string Endpoint { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
